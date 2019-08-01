1c8330853de7b8127d22d9b723110f5665ce6c49
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-08-01 17:07:25 -0500

    Basics of EF

--- Certification70-487_Framework4.6/Unit 2/Entity Framework/LinqToEFTest.cs
+++ Certification70-487_Framework4.6/Unit 2/Entity Framework/LinqToEFTest.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
+using System.Data;
+using System.Data.EntityClient;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Certification70_487_Framework4._6.Unit_2.Entity_Framework
 {
+    /// <summary>
+    /// This test is only for Entity Framework 5 based on .Net Framework 4.6
+    /// </summary>
     [TestClass]
     public class LinqToEFTest
     {
         private CertificationCourseEntities dbContext;
 
         [TestInitialize]
-        public void SetUp() {
+        public void SetUp()
+        {
             this.dbContext = new CertificationCourseEntities();
         }
 
@@ -33,6 +38,97 @@ namespace Certification70_487_Framework4._6.Unit_2.Entity_Framework
             var numberOfChanges = dbContext.SaveChanges();
 
             Assert.IsTrue(numberOfChanges == 1, "Change wasn´t applied");
+        }
+
+        [TestMethod]
+        public void LinqToEntityTest()
+        {
+            dbContext.Employee.Add(new Employee
+            {
+                FirstName = "Geiser",
+                LastName = "Aragon",
+                BirtthDate = new DateTime(1992, 07, 11),
+                Genre = "M"
+            });
+
+            dbContext.Employee.Add(new Employee
+            {
+                FirstName = "Dayana",
+                LastName = "Gaellgos",
+                BirtthDate = new DateTime(1997, 07, 20),
+                Genre = "M"
+            });
+
+            IQueryable query = from e in dbContext.Employee where e.BirtthDate.Year < 1995 select e.FirstName;
+
+            Debug.WriteLine(query.ToString());
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void TransactionTest(bool requiredRollback)
+        {
+            try
+            {
+                using (var scope = new TransactionScope())
+                {
+                    var employee = new Employee
+                    {
+                        FirstName = "Dafne",
+                        LastName = "Aragon",
+                        BirtthDate = new DateTime(2012, 11, 09),
+                        Genre = "F"
+                    };
+
+                    dbContext.Employee.Add(employee);
+                    var numberOfChanges = dbContext.SaveChanges();
+
+                    if (requiredRollback)
+                    {
+                        throw new Exception("Test rollback");
+                    }
+
+                    var exists = dbContext.Employee.Any(x => x.FirstName == "Dafne");
+                    Assert.IsTrue(exists, "Dafne is NOT in the database");
+
+                    scope.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                var exists = dbContext.Employee.Any(x => x.FirstName == "Dafne");
+                Assert.IsFalse(exists, "Dafne is in the database");
+            }
+        }
+
+        // **** It requires ObjectContextt instead DBContext (basicly it is obsolete). 
+        //[TestMethod]
+        //public void CopiledQUeryTest()
+        //{
+        //    var compiledQuery = CompiledQuery.Compile<CertificationCourseEntities, int, Employee>(
+        //        (CertificationCourseEntities dbContext, int employyeId) => {
+        //            return dbContext.Employee.FirstOrDefault();
+        //        });
+        //}
+
+        [TestMethod]
+        public void SLQueryTest()
+        {
+            using (var connection = new EntityConnection("name=CertificationCourseEntities"))
+            {
+                connection.Open();
+                EntityCommand command = new EntityCommand("SELECT VALUE p FROM CertificationCourseEntities.Employee AS p", connection);
+                using (var reader = command.ExecuteReader(System.Data.CommandBehavior.SequentialAccess | System.Data.CommandBehavior.CloseConnection))
+                {
+                    while (reader.Read())
+                    {
+                        string firstname = reader.GetString(1);
+                        string lastname = reader.GetString(2);
+                        Console.WriteLine("{0} {1}", firstname, lastname);
+                    }
+                }
+            }
         }
     }
 }
