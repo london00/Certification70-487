ea866ead1dccabb3fd4b883c852ef87486ac609c
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-08-01 11:11:52 -0500

    Add entity model for unit 2

--- Certification70-487_Framework4.6/Unit 1/XML/XmlTest.cs
+++ Certification70-487_Framework4.6/Unit 1/XML/XmlTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Certification70_487_Framework4._6.Unit_1.XML
@@ -7,44 +9,6 @@ namespace Certification70_487_Framework4._6.Unit_1.XML
     [TestClass]
     public class XmlTest
     {
-        [TestMethod]
-        [DataRow("WriteXMByXMLDocumentLTest.xml")]
-        public void WriteXMByXMLDocumentLTest(string fileName)
-        {
-            var customersList = new[]{
-                new ExamplePOCOClass("Geiser", "A", "Aragon"),
-                new ExamplePOCOClass("Dayana", "N", "Gallegos"),
-                new ExamplePOCOClass("Dafne", "N", "Aragon"),
-                new ExamplePOCOClass("Dayana", "S", "Aragon")
-            };
-
-            XmlDocument xmlWriter = new XmlDocument();
-            {
-                XmlElement customersNode = xmlWriter.CreateElement("Customers");
-               
-                foreach (var customerElement in customersList)
-                {
-                    XmlElement customerNode = xmlWriter.CreateElement("Customer");
-
-                    XmlElement FirstNameNode = xmlWriter.CreateElement("FirstName");
-                    FirstNameNode.InnerText = customerElement.FirstName;
-                    customerNode.AppendChild(FirstNameNode);
-
-                    XmlElement MiddleInitialNode = xmlWriter.CreateElement("MiddleInitial");
-                    MiddleInitialNode.InnerText = customerElement.MiddleInitial;
-                    customerNode.AppendChild(MiddleInitialNode);
-
-                    XmlElement LastNameNode = xmlWriter.CreateElement("LastName");
-                    LastNameNode.InnerText = customerElement.LastName;
-                    customerNode.AppendChild(LastNameNode);
-
-                    customersNode.AppendChild(customerNode);
-                }
-            }
-
-            xmlWriter.Save(fileName);
-        }
-
         [TestMethod]
         [DataRow("WriteXMLTest.xml")]
         public void WriteXMLTest(string fileName)
@@ -76,8 +40,81 @@ namespace Certification70_487_Framework4._6.Unit_1.XML
         }
 
         [TestMethod]
+        [DataRow("WriteXMByXMLDocumentLTest.xml")]
+        public void WriteXMByXMLDocumentLTest(string fileName)
+        {
+            var customersList = new[]{
+                new ExamplePOCOClass("Geiser", "A", "Aragon"),
+                new ExamplePOCOClass("Dayana", "N", "Gallegos"),
+                new ExamplePOCOClass("Dafne", "N", "Aragon"),
+                new ExamplePOCOClass("Dayana", "S", "Aragon")
+            };
+
+            XmlDocument xmlWriter = new XmlDocument();
+            {
+                XmlElement customersNode = xmlWriter.CreateElement("Customers");
+
+                foreach (var customerElement in customersList)
+                {
+                    XmlElement customerNode = xmlWriter.CreateElement("Customer");
+
+                    XmlElement FirstNameNode = xmlWriter.CreateElement("FirstName");
+                    FirstNameNode.InnerText = customerElement.FirstName;
+                    customerNode.AppendChild(FirstNameNode);
+
+                    XmlElement MiddleInitialNode = xmlWriter.CreateElement("MiddleInitial");
+                    MiddleInitialNode.InnerText = customerElement.MiddleInitial;
+                    customerNode.AppendChild(MiddleInitialNode);
+
+                    XmlElement LastNameNode = xmlWriter.CreateElement("LastName");
+                    LastNameNode.InnerText = customerElement.LastName;
+                    customerNode.AppendChild(LastNameNode);
+
+                    customersNode.AppendChild(customerNode);
+                }
+            }
+
+            xmlWriter.Save(fileName);
+        }
+
+        [TestMethod]
+        [DataRow("WriteXMByLinqTest.xml")]
+        public void WriteXMByLinqTest(string fileName)
+        {
+            XElement customersXML = new XElement(
+                "Customers",
+                new XElement("Customer",
+                    new XElement("FirstName", "Geiser"),
+                    new XElement("MiddleInitial", "A"),
+                    new XElement("LastName", "Aragon")
+                ),
+                new XElement("Customer",
+                    new XElement("FirstName", "Dayana"),
+                    new XElement("MiddleInitial", "N"),
+                    new XElement("LastName", "Gallegos")
+                ),
+                new XElement("Customer",
+                    new XElement("FirstName", ""),
+                    new XElement("MiddleInitial", ""),
+                    new XElement("LastName", "")
+                ),
+                new XElement("Customer",
+                    new XElement("FirstName", "Dayana"),
+                    new XElement("MiddleInitial", "S"),
+                    new XElement("LastName", "Aragon")
+                )
+            );
+
+            // Get an element
+            var firstName = customersXML.Elements("Customer").First().Element("FirstName").ToString();
+
+            customersXML.Save(fileName);
+        }
+
+        [TestMethod]
         [DataRow("WriteXMLTest.xml")]
         [DataRow("WriteXMByXMLDocumentLTest.xml")]
+        [DataRow("WriteXMByLinqTest.xml")]
         public void ReadXMLTest(string fileName)
         {
             using (var xmlReader = XmlReader.Create("customer.xml"))
@@ -112,7 +149,7 @@ namespace Certification70_487_Framework4._6.Unit_1.XML
                             break;
                     }
                 }
-                
+
             }
         }
     }
---
+++ Certification70-487_Framework4.6/Unit 2/Entity Framework/LinqToEFTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification70_487_Framework4._6.Unit_2.Entity_Framework
+{
+    [TestClass]
+    public class LinqToEFTest
+    {
+        private CertificationCourseEntities dbContext;
+
+        [TestInitialize]
+        public void SetUp() {
+            this.dbContext = new CertificationCourseEntities();
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            var employee = new Employee
+            {
+                FirstName = "Geiser",
+                LastName = "Aragon",
+                BirtthDate = new DateTime(1992, 07, 11),
+                Genre = "M"
+            };
+
+            dbContext.Employee.Add(employee);
+
+            var numberOfChanges = dbContext.SaveChanges();
+
+            Assert.IsTrue(numberOfChanges == 1, "Change wasn´t applied");
+        }
+    }
+}
