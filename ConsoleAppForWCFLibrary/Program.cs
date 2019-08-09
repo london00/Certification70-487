625aa927b3eaee0535511fc66604c541bc33ab11
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-08-09 17:20:16 -0500

    Setup a WCF service by code

---
+++ ConsoleAppForWCFLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using WcfServiceApplication;
+
+namespace ConsoleAppForWCFLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let the things ready...");
+            var uri = new Uri(("http://localhost:8081/"));
+            using (var hoster = new ServiceHost(typeof(MyFirstService), uri))
+            {
+                #region Add Service
+                hoster.AddServiceEndpoint(typeof(IMyFirstService), new WSHttpBinding(SecurityMode.None, true), nameof(MyFirstService));
+                #endregion
+
+                #region Add metadata
+                var serviceMetadataBehavior = new ServiceMetadataBehavior();
+                serviceMetadataBehavior.HttpGetEnabled = true;
+                hoster.Description.Behaviors.Add(serviceMetadataBehavior);
+                hoster.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName, MetadataExchangeBindings.CreateMexHttpBinding(), "mexBindingHttp");
+                #endregion
+
+                hoster.Open();
+                Console.WriteLine("Ready!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
