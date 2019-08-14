14bfb9e145716841599cccbd0c1df7ca3a1c1f2a
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-08-14 09:17:12 -0500

    Azure Bus Relay - WCF Relay

---
+++ ConsoleAppHost_AzureBusRelay/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFRelay_WcfServiceLibrary;
+
+namespace ConsoleAppHost_AzureBusRelay
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Endpoint = sb://servicerelaywcf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jKp3MftOIwWfpN+mRQv3KpIdlGbuSIVbKqpF4Efdrz8=
+            string scheme = "sb";
+            string serviceNamespace = "ServiceRelayWCF";
+            string servicePath = "https://ServiceRelayWCF.servicebus.windows.net/mytcprelay";
+            Uri address = ServiceBusEnvironment.CreateServiceUri(scheme, serviceNamespace, servicePath);
+
+            using (ServiceHost sh = new ServiceHost(typeof(Service1), address))
+            {
+                var serviceEndPoint = sh.AddServiceEndpoint(typeof(IService1), new NetTcpRelayBinding(), address);
+
+                #region Create token provider
+                string policy = "RootManageSharedAccessKey";
+                string accessKey = "jKp3MftOIwWfpN+mRQv3KpIdlGbuSIVbKqpF4Efdrz8=";
+                var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(policy, accessKey);
+                #endregion
+
+                serviceEndPoint.Behaviors.Add(new TransportClientEndpointBehavior(tokenProvider));
+
+                sh.Open();
+
+                Console.WriteLine("Press ENTER to close");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ WCFClientConsoleApplication_AzureBusRelay/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFClientConsoleApplication_AzureBusRelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scheme = "sb";
+            string serviceNamespace = "ServiceRelayWCF";
+            string servicePath = "https://ServiceRelayWCF.servicebus.windows.net/mytcprelay";
+
+            ServiceBusEnvironment.SystemConnectivity.Mode = ConnectivityMode.AutoDetect;
+
+            var cf = new ChannelFactory<IRechargeChannel>(
+                new NetTcpRelayBinding(),
+                new EndpointAddress(
+                    ServiceBusEnvironment.CreateServiceUri(scheme, serviceNamespace, servicePath)
+                ));
+
+            #region Create token provider
+            string policy = "RootManageSharedAccessKey";
+            string accessKey = "jKp3MftOIwWfpN+mRQv3KpIdlGbuSIVbKqpF4Efdrz8=";
+            var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(policy, accessKey);
+            #endregion
+
+            cf.Endpoint.Behaviors.Add(new TransportClientEndpointBehavior(tokenProvider));
+
+            using (IRechargeChannel ch = cf.CreateChannel())
+            {
+                Console.WriteLine(ch.GetData(1));
+            }
+        }
+    }
+
+    [ServiceContract(Namespace = "https://ServiceRelayWCF.servicebus.windows.net/mytcprelay")]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+
+    interface IRechargeChannel : IService1, IClientChannel { }
+}
