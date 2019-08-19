37028192b3fa84caa8bca4af7b76c50633baf82e
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-08-19 11:40:19 -0500

    Async actions in Web API

--- WebApiFromScratch/Controllers/PeopleController.cs
+++ WebApiFromScratch/Controllers/PeopleController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using WebApiFromScratch.Filters;
 using WebApiFromScratch.Models;
 
 namespace WebApiFromScratch.Controllers
@@ -27,6 +30,7 @@ namespace WebApiFromScratch.Controllers
 
         [HttpGet]
         [ActionName("GetPersonByName")]
+        [CustomFilter]
         public Person Person([FromUri] string name)
         {
             var person = personService.GetExamplePeople().FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
@@ -51,6 +55,24 @@ namespace WebApiFromScratch.Controllers
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
 
+
+            return person;
+        }
+
+        [HttpGet]
+        public async Task<Person> PersonByNameWithoutCustomActionNameAsync([FromUri] string name)
+        {
+            var person = personService.GetExamplePeople().FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+
+            if (person == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            // Use the thread during 10 seconds. As the actions is async it is possible to make parallel requests.
+            await Task.Factory.StartNew(() => {
+                Thread.Sleep(10000);
+            });
 
             return person;
         }
---
+++ WebApiFromScratch/Filters/CustomFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Http.Filters;
+
+namespace WebApiFromScratch.Filters
+{
+    public class CustomFilter: ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            Debug.WriteLine(actionExecutedContext.ActionContext.ActionDescriptor.ActionName + " has been executed" );
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
