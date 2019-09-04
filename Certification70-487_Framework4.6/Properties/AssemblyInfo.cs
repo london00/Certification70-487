e2c0763c465089e7520486575eeb2399ae08c50d
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-09-04 10:35:09 -0500

    Sign assembly

--- Certification70-487_Framework4.6/Properties/AssemblyInfo.cs
+++ Certification70-487_Framework4.6/Properties/AssemblyInfo.cs
@@ -3,18 +3,18 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Certification70-487_Framework4.6")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This assembly contains unit testing related with 70-487 certification")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Certification70-487_Framework4.6")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©GeiserDev  2019")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("574665aa-cbd0-4cd8-b1e4-d89f08428512")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Certification70-487_Framework4.6/Unit 1/AzureStorage/CosmosDBTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Certification70_487_Framework4._6.Unit_1.AzureStorage
+{
+    [TestClass]
+    public class CosmosDBTest
+    {
+        [TestMethod]
+        public void CreateDatabaseTest()
+        {
+
+            Assert.Fail();
+        }
+    }
+}
