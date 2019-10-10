86bba5ed87cfe90e29e4a270a5fb27a830159b04
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-10-10 15:05:13 -0500

    Example of use of Cryptography library

---
+++ Certification70-487_Framework4.6/Cryptography/EncriptionTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Certification70_487_Framework4._6.Cryptography
+{
+    [TestClass]
+    public class EncriptionTest
+    {
+        [TestMethod]
+        [DataRow("Geiser")]
+        public void AesEncryption(string text)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).    
+                // Same key must be used in encryption and decryption    
+                using (AesManaged aes = new AesManaged())
+                {
+                    // Encrypt string    
+                    byte[] encrypted = AESHelper.Encrypt(text, aes.Key, aes.IV);
+
+                    // Print encrypted string    
+                    Debug.WriteLine($"Encrypted data: {Encoding.UTF8.GetString(encrypted)}");
+
+                    // Decrypt the bytes to a string.    
+                    string decrypted = AESHelper.Decrypt(encrypted, aes.Key, aes.IV);
+
+                    // Print decrypted string. It should be same as raw data    
+                    Debug.WriteLine($"Decrypted data: {decrypted}");
+
+                    Assert.AreEqual(text, decrypted);
+                }
+            }
+            catch (Exception exp)
+            {
+                Debug.WriteLine(exp.Message);
+            }
+        }
+
+        [TestMethod]
+        [DataRow("Geiser")]
+        public void RSAEncryption(string text)
+        {
+            //Create byte arrays to hold original, encrypted, and decrypted data.
+            byte[] dataToEncrypt = Encoding.UTF8.GetBytes(text);
+
+            // Create a new instance of RSACryptoServiceProvider to generate public and private key data.
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                #region Generate public and Private key
+
+                var publicKey = RSA.ToXmlString(false);
+                var privateKey = RSA.ToXmlString(true);
+
+                #endregion
+
+                #region Encrypt and decrypt
+
+                // Pass the data to ENCRYPT, the public key information (using RSACryptoServiceProvider.ExportParameters(false), and a boolean flag specifying no OAEP padding.
+                byte[] encryptedData = RSAHelper.RSAEncrypt(dataToEncrypt, publicKey);
+
+                // Pass the data to DECRYPT, the private key information  (using RSACryptoServiceProvider.ExportParameters(true), and a boolean flag specifying no OAEP padding.
+                byte[] decryptedData = RSAHelper.RSADecrypt(encryptedData, privateKey);
+
+                #endregion
+
+                // Display the decrypted plaintext to the console. 
+                Debug.WriteLine($"Decrypted plaintext: {Encoding.UTF8.GetString(decryptedData)}");
+
+                Assert.AreEqual(text, Encoding.UTF8.GetString(decryptedData));
+            }
+        }
+    }
+}
---
+++ Certification70-487_Framework4.6/Cryptography/Helpers/RSAHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Certification70_487_Framework4._6.Cryptography
+{
+    public class RSAHelper
+    {
+        const bool OAEPPADDING = false;
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, string publicKey)
+        {
+            try
+            {
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This only needs toinclude the public key information.
+                    RSA.FromXmlString(publicKey);
+
+                    //Encrypt the passed byte array and specify OAEP padding. OAEP padding is only available on Microsoft Windows XP or later.  
+                    return RSA.Encrypt(DataToEncrypt, OAEPPADDING);
+                }
+            }
+            //Catch and display a CryptographicException to the console.
+            catch (CryptographicException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+         
+            return null;
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, string privateKey)
+        {
+            try
+            {
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This needs to include the private key information.
+                    RSA.FromXmlString(privateKey);
+
+                    //Decrypt the passed byte array and specify OAEP padding. OAEP padding is only available on Microsoft Windows XP or later.  
+                    return RSA.Decrypt(DataToDecrypt, OAEPPADDING);
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            
+            return null;
+        }
+    }
+}
